717a4153efc3dcf11c25d54f01ec9205f4cf5a15
gauthamav10 <goutham.zedsoft@gmail.com>
2026-03-26 02:00:40 +0530

    bug fixed

--- src/components/Product-Section/ProductPageSideScroll.jsx
+++ src/components/Product-Section/ProductPageSideScroll.jsx
@@ -1,21 +1,16 @@
-import React from 'react'
-import { productPageLeftScrollImageData } from '../../utils/ProductPageSectionData'
+import React from 'react';
+import { productPageLeftScrollImageData } from '../../utils/productPageSectionData';
 
-const ProductPageSideScroll = ({newObj}) => {
-
- const newArr = [newObj,...productPageLeftScrollImageData]
- 
-
+const ProductPageSideScroll = ({ newObj }) => {
+  const newArr = [newObj, ...productPageLeftScrollImageData];
 
   return (
-    <div className='column gap-2 h-[450px] max-w-[90px] overflow-y-auto hide-scrollbar' >
-      
-     {newArr.map((item ) => (
-      <img className='w-[50px] object-cover' key={item.id} src={item.image} alt="" />
-     ))}
-      
+    <div className="column gap-2 h-[450px] max-w-[90px] overflow-y-auto hide-scrollbar">
+      {newArr.map(item => (
+        <img className="w-[50px] object-cover" key={item.id} src={item.image} alt="" />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default ProductPageSideScroll
+export default ProductPageSideScroll;
